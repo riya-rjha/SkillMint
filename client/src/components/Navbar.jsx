d400f17f7629abcb96bbc539ec8f257c8834503f
riya-rjha <riyaranjanjha2006@gmail.com>
2024-10-05 21:20:09 +0530

    Changes in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import SkillMint from '../Resources/Skillmint.png';
+import React from "react";
+import SkillMint from "../Resources/Skillmint.png";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between items-center p-4 bg-green-800 shadow-md">
-      <div className="text-green-500 text-xl font-semibold"><img src={SkillMint} className='w-20 rounded-full' /></div>
-      <div className="flex gap-14">
-        {['Home', 'About', 'Courses', 'Services', 'Blogs'].map((item) => (
-          <div key={item} className="text-green-950 text-lg  bg-blue-100 hover:bg-slate-100 transition-all delay-75 p-3 rounded-lg hover:text-green-500 cursor-pointer">
+    <div className="flex justify-between sticky top-0 z-50  items-center p-3 bg-white shadow-md backdrop-blur-lg ">
+      <div className="text-green-500 text-xl font-semibold">
+        <img
+          src={SkillMint}
+          className="w-20 rounded-full"
+          alt="SkillMint Logo"
+        />
+      </div>
+      <div className="flex gap-8">
+        {["Home", "About", "Courses", "Services"].map((item) => (
+          <div
+            key={item}
+            className="text-black text-base hover:text-green-500 cursor-pointer transition-colors duration-200"
+          >
             {item}
           </div>
         ))}
-        <div className="text-green-800 text-lg bg-green-100 hover:bg-slate-100 transition-all delay-75 p-3 rounded-lg">Login/Register</div>
+        <div className="text-black text-base hover:text-green-500 cursor-pointer transition-colors duration-200">
+          Connect Wallet
+        </div>
       </div>
     </div>
   );

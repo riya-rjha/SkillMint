afc91274319f8f9de9aae0eed8e08731b06a2fe4
Aditya Monga <121632961+Aditya00013@users.noreply.github.com>
2024-10-06 15:21:46 +0530

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-
-
-
-const fadeIn = {
-  initial: { opacity: 0, y: 20 },
-  animate: { opacity: 1, y: 0 },
-  transition: { duration: 0.6 }
-};
 
 
 function Navigation() {
